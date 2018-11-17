388635f6293098df3e319b223bc36b1a3286b5e9
shalevpenker97 <45102920+shalevpenker97@users.noreply.github.com>
2018-11-17 18:54:00 +0200

    Create testcode.py

---
+++ testcode.py
@@ -0,0 +1,26 @@
+import unittest
+import app
+
+class MyTestClass(unittest.TestCase):
+    def setUp(self):
+        # creates a test client
+      #  self.app = app.test_client()
+      app.app.config['TESTING']=True
+      self.app = app.app.test_client()
+        # propagate the exceptions to the test client
+      #  self.app.testing = True 
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the response data
+        self.assertEqual(result.data, "Hello World!!!")
