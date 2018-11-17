e6ec6c12542d7d7cb036229e9784d688a1bc248b
shalevpenker97 <45102920+shalevpenker97@users.noreply.github.com>
2018-11-17 19:30:38 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -23,4 +23,4 @@ class MyTestClass(unittest.TestCase):
         result = self.app.get('/')
 
         # assert the response data
-        self.assertEqual(result.data, "Hello World!!!")
+        self.assertEqual(result.data, "Hello Wold!!!")
